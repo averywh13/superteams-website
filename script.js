214be5f6ac11c09a89507e74b323f90f3f0f833e
averywh13 <awekslerhart@gmail.com>
2024-05-07 14:27:24 -0400

    work with kilgore

--- script.js
+++ script.js
@@ -1,36 +1,74 @@
 // team member data
 const teamMembers = [
-    {
-        name: 'Lionel Messi',
-        age: 36,
-        activePlayer: true,
-        position: 'forward',
-        strengths: 'Ball Control',
-        weaknesses: 'heading ability',
-        skills: ['dribbling', 'shooting', 'passing'],
-        biography: 'blah blah blah blah'
-    }
-]
+  {
+    name: "Lionel Messi",
+    age: 36,
+    activePlayer: true,
+    position: "forward",
+    strengths: "Ball Control",
+    weaknesses: "heading ability",
+    skills: ["dribbling", "shooting", "passing"],
+    biography: "blah blah blah blah",
+  },
+  {
+    name: "Lionel Messi",
+    age: 36,
+    activePlayer: true,
+    position: "defender",
+    strengths: "Ball Control",
+    weaknesses: "heading ability",
+    skills: ["dribbling", "shooting", "passing"],
+    biography: "blah blah blah blah",
+  },
+  {
+    name: "Lionel Messi",
+    age: 36,
+    activePlayer: true,
+    position: "mid",
+    strengths: "Ball Control",
+    weaknesses: "heading ability",
+    skills: ["dribbling", "shooting", "passing"],
+    biography: "blah blah blah blah",
+  },
+];
 
-function generateTeamCards(){
-const teamCardsContainer = document.getElementById('teamCards')
+function generateTeamCards() {
+  const teamCardsContainer = document.getElementById("teamCards");
 
-teamMembers.forEach(member => {
-    const card = document.createElement('div')
-    card.classList.add('col-md-4')
+  teamMembers.forEach((member) => {
+    const card = document.createElement("div");
+    card.classList.add("col-md-4");
+
+// style the BGColor of my card based on position
+let backgroundColor
+switch(member.position.toLowerCase()){
+    case 'forward':
+        backgroundColor = "red"
+        break
+    case 'mid':
+        backgroundColor = "green"
+        break
+    case 'defender':
+        backgroundColor = "blue"
+        break
+    default:
+        backgroundColor: "grey"
+}
+
+card.style.backgroundColor = backgroundColor
+
     card.innerHTML = `
     <div class="card">
         <div class="card-header">
         ${member.name}
         </div>
+        <div class="card-body" style="background-color: ${backgroundColor};">
+        <p><strong>Position:</strong> ${member.position}</p>
+        </div>
     </div>
-    `
-teamCardsContainer.appendChild(card)
-
-});
-
-
-
+    `;
+    teamCardsContainer.appendChild(card);
+  });
 }
 
-window.onload = generateTeamCards()
+window.onload = generateTeamCards();
