a82bdcaf1cc802fa2f9d5075cc95a22d00b5fa53
averywh13 <awekslerhart@gmail.com>
2024-05-13 21:45:25 -0400

    added 3 ricks - need to figure out pictures

--- script.js
+++ script.js
@@ -1,64 +1,103 @@
-// team member data ------ 1ST TEAM WILL HAVE ALL TYPES OF RICKS FROM RICK AND MORTY
+// create a team of types of ricks
+
 const teamMembers = [
   {
-    name: "Morty",
-    age: 14,
-    species: "human",
-    status: "alive",
-    strengths: "Ball Control",
-    weaknesses: "heading ability",
-    skills: ["dribbling", "shooting", "passing"],
-    biography: "blah blah blah blah", 
+    name: 'Rick',
+    age: 70,
+    species: 'Human',
+    occupation: 'Scientist',
+    type: "Rick",
+    status: 'Alive',
+    origin: 'Earth (Dimension: C-137)',
+    strengths: 'blah blah blah',
+    weaknesses: 'blah blah blah',
+    skills: ['blah', 'blah', 'blah'],
+    biography:
+      'blah blah blah blah'
+    
   },
   {
-    name: "Rick",
-    age: 36,
-    activePlayer: true,
-    position: "defender",
-    strengths: "Ball Control",
-    weaknesses: "heading ability",
-    skills: ["dribbling", "shooting", "passing"],
-    biography: "blah blah blah blah",
+    name: 'Black Magic Rick',
+    age: 70,
+    species: 'Human',
+    occupation: 'Magician',
+    type: "Rick",
+    status: 'Deceased',
+    origin: 'Earth (Dimension: Unknown)',
+    strengths: 'blah blah blah',
+    weaknesses: 'blah blah blah',
+    skills: ['blah', 'blah', 'blah'],
+    biography:
+      'blah blah blah blah'
   },
-];
+  {
+    name: 'Wasp Rick',
+    age: 70,
+    species: 'Wasp',
+    occupation: 'Scientist',
+    type: "Rick",
+    status: 'Alive',
+    origin: 'Earth (Wasp Universe)',
+    strengths: 'blah blah blah',
+    weaknesses: 'blah blah blah',
+    skills: ['blah', 'blah', 'blah'],
+    biography:
+      'blah blah blah blah'
+  }
+]
 
-function generateTeamCards() {
-  const teamCardsContainer = document.getElementById("teamCards");
+function generateTeamCards () {
+  const teamCardsContainer = document.getElementById('teamCards')
 
-  teamMembers.forEach((member) => {
-    const card = document.createElement("div");
-    card.classList.add("col-md-4");
+  teamMembers.forEach(member => {
+    const card = document.createElement('div')
+    card.classList.add('col-md-4')
 
-// style the BGColor of my card based on position
-let backgroundColor
-switch(member.position.toLowerCase()){
-    case 'forward':
-        backgroundColor = "red"
+    //style the BGColor of my card based on pos
+    let backgroundColor
+
+    switch (member.type.toLowerCase()) {
+      case 'rick':
+        backgroundColor = 'red'
         break
-    case 'mid':
-        backgroundColor = "green"
+      case 'morty':
+        backgroundColor = 'green'
         break
-    case 'defender':
-        backgroundColor = "blue"
+      case 'summer':
+        backgroundColor = 'blue'
         break
-    default:
-        backgroundColor: "grey"
-}
-
-card.style.backgroundColor = backgroundColor
+      default:
+        backgroundColor = 'gray'
+    }
+    
 
     card.innerHTML = `
-    <div class="card">
-        <div class="card-header">
-        ${member.name}
-        </div>
-        <div class="card-body" style="background-color: ${backgroundColor};">
-        <p><strong>Position:</strong> ${member.position}</p>
-        </div>
-    </div>
-    `;
-    teamCardsContainer.appendChild(card);
-  });
+            <div class="card">
+                <div class = "card-header">
+                    ${member.name}
+                </div>
+                <div id="cardBody" class="card-body" style="background-color:${backgroundColor};">
+                    <p><strong>Age:</strong> ${member.age}</p>
+                    <p><strong>Species:</strong> ${member.species}</p>
+                    <p><strong>Occupation:</strong> ${member.occupation}</p>
+                    <p><strong>Type:</strong> ${member.type}</p>
+                    <p><strong>Status:</strong> ${member.status}</p>
+                    <p><strong>Origin:</strong> ${member.origin}</p>
+                    <p><strong>Strengths:</strong> ${member.strengths}</p>
+                    <p><strong>Weaknesses:</strong> ${member.weaknesses}</p>
+                    <p><strong>Skills:</strong> ${member.skills}</p>
+                    <p><strong>Biography:</strong> ${member.biography}</p>
+                </div>
+            </div>
+        `
+//will update the BG style on each child of the card
+    for (let i = 0; i < card.children.length; i++) {
+      card.children[i].style.backgroundColor = backgroundColor
+    }
+
+    teamCardsContainer.appendChild(card)
+  })
 }
 
-window.onload = generateTeamCards();
+window.onload = generateTeamCards()
+
